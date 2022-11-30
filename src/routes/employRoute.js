54dd2279047d122382fdf69c1aa4c5d7de4477b1
JeonghyeonK <6843wjdgus@naver.com>
2022-11-30 21:34:31 +0900

    직원 전부 출력 api, 직원 id로 검색 api 생성

--- src/routes/employRoute.js
+++ src/routes/employRoute.js
@@ -1,45 +1,61 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const mongoose = require("mongoose");
-const { Empl } = require('../models/Employ');
-const { Employ } = require('../classes/emplClass')
+const { Empl } = require("../models/Employ");
+const { Employ } = require("../classes/emplClass");
 
-const http = require('http');
-const fs = require('fs'); 
-var path = require('path');
+const http = require("http");
+const fs = require("fs");
+var path = require("path");
 
-const express = require('express');
+const express = require("express");
 const employRouter = express.Router();
 
-const ejs = require('ejs');
+const ejs = require("ejs");
 var app = express();
 
-employRouter.use(express.json())
-employRouter.use(express.urlencoded({extended:false}));
+employRouter.use(express.json());
+employRouter.use(express.urlencoded({ extended: false }));
 
 employRouter.use(express.static(__dirname));
 
-employRouter.post('/post', async(req, res) => {
-    try {
-        console.log("posting information");
-        console.log(req.body);
+employRouter.get("/:worNo", async (req, res) => {
+  let { worNo } = req.params;
+  const employFound = await Empl.find({ worNo: worNo });
+  console.log("employFound : " + employFound);
+  return res.send({ employFound });
+});
 
-        let { worNo, worId, worPwd, worName, worTel, worPosition } = req.body;
+employRouter.get("/", async (req, res) => {
+  try {
+    const employFound = await Empl.find({});
+    res.status(200).send(employFound);
+  } catch {
+    console.log(err);
+    return res.status(500).send({ err: err.message });
+  }
+});
 
-        if (!worNo || !worId || !worPwd || !worName || !worTel || !worPosition)
-            return res.status(400).send({ err: "All informations are required" });
+employRouter.post("/post", async (req, res) => {
+  try {
+    console.log("posting information");
+    console.log(req.body);
 
-        // 데이터베이스에 저장
-        const worker = new Empl(req.body);
-        await worker.save();
-        
-        return res.send({ worker });
-          
-    } catch(err) {
-        console.log(err);
-        return res.status(500).send({ err: err.message });
-    }
+    let { worNo, worId, worPwd, worName, worTel, worPosition } = req.body;
+
+    if (!worNo || !worId || !worPwd || !worName || !worTel || !worPosition)
+      return res.status(400).send({ err: "All informations are required" });
+
+    // 데이터베이스에 저장
+    const worker = new Empl(req.body);
+    await worker.save();
+
+    return res.send({ worker });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ err: err.message });
+  }
 });
 
 module.exports = {
-    employRouter
-}
+  employRouter,
+};
